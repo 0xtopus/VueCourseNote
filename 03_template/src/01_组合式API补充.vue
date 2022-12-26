2bffab320fb7be5ba7de8da3d479754c81694af4
cyber_octopus <unavailabe>
2022-12-26 22:03:57 +0800

    Vue code 12.26.22

---
+++ 03_template/src/01_组合式API补充.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { computed,ref,reactive } from "vue";
+
+const msg = ref("hahah")
+
+//对象,如果全部属性都是响应式，那么推荐用 reactive
+const obj = ref({
+  name:"tommyboy",
+  age:20
+})
+
+//对象，如果不是全部属性都要响应式，则
+const obj2 = {
+  name: "tom",
+  age: ref(19)  //只暴露需要响应式的属性
+}
+
+// 这个时候在template里不能直接访问，需要 obj2.age.value 才能访问
+// 或者使用这种方法：
+const { age } = obj2
+
+
+function changeMsg() {
+  // ref要写.value!
+  msg.value = "hihihi!"
+}
+
+//computed计算属性
+const appendMsg = computed(() => {
+  //不需要使用this，可以使用箭头函数了
+  return msg.value+"== news arrived"
+})
+
+</script>
+
+
+
+<template>
+  <!-- ref在模版里会自动解包 -->
+  <h1>{{ msg }}</h1>
+  <button @click="changeMsg">click me!</button>
+  <hr>
+  <h2>{{ obj.name }}</h2>
+  <h2>{{ obj.age }}</h2>
+  <hr>
+  <h2>{{ age }}</h2>
+  <hr>
+  <h2>{{ appendMsg }}</h2>  
+</template>
---
+++ 03_template/src/02_模版的基本语法.vue
@@ -0,0 +1,29 @@
+<script setup>
+  const msg = "i love vue!"
+  const html = "<h2>insert snippets</h2>"
+</script>
+
+
+<template>
+  <!-- 
+    - 在模版中可以直接访问组件声明的变量
+    - 一些全局对象也可以访问
+    - 可以通过app对象向Vue添加一些全局变量
+        app.config.globalProperties.alert = alert.bind(this)
+    - 使用插值（{{  }}）时，只能使用表达式（有返回值的语句）
+        比如if语句就写不了
+        
+    - 插值实际上就是在修改元素的textContent
+    
+    指令：
+      - 指令模版中为标签设置的一些特殊属性，它可以用来设置标签如何显示内容
+      - v-text 将表达式作为textContent插入，作用同{{  }}
+      - v-html 将表达式作为元素的innerHTML插入，有XSS注入的风险
+    -->
+  <h1>{{ msg }}</h1>
+  <h1>{{ Math.random() }}</h1>
+  <!-- <h2>{{ alert(123) }}</h2> -->
+  <hr>
+  <div v-text = "html"></div>
+  <div v-html = "html"></div>
+</template>
