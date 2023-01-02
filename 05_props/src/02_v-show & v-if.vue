6b4fe4622a305e2bb433e98b3681726a58af496a
cyber_octopus <gsy@gsy123mailbox.com>
2023-01-02 21:11:17 +0800

    Vue code 1.2.23

---
+++ 05_props/src/02_v-show & v-if.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+  import { ref } from "vue";
+
+  const isShow = ref(true)
+</script>
+
+
+<template>
+  <!-- 
+    v-show: 修改CSS的display ： 
+      性能好一点，但需要对所有组件进行初始化
+    
+    v-if:   删除/添加元素：     
+      性能差一点，但对不显示的可以不初始化
+
+      可以结合v-else使用
+
+      可以结合template使用
+  -->
+  <h1>V-SHOW & V-IF</h1>
+  <button @click="isShow = !isShow">show show</button>
+  <div v-if="isShow">
+  <!-- <div v-show="isShow"> -->
+    <h2 >show me</h2>
+  </div>
+  <div v-else>
+    <h2>yet another msg</h2>
+  </div>
+  <template v-if="isShow">
+    <h2>wawaw</h2>
+    <h3>kakaka</h3>
+  </template>
+</template>
+
+
+<style scoped>
+
+</style>
